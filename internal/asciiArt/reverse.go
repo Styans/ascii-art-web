98aeb6639e32bc0dd6851fe5837bd59866da180f
styan <milkaxd657@gmail.com>
2023-11-18 16:38:43 +0600

    update a reverse-ascii

--- internal/asciiArt/reverse.go
+++ internal/asciiArt/reverse.go
@@ -23,6 +23,25 @@ func (art *ArtObjects) ReverseAscii() error {
 		mapAscii[i] = j
 		j += 9
 	}
+	text, err := FUCK(arr, art.Reverse.Standard, mapAscii)
+	if err == nil {
+		art.Result = text[:len(text)-1]
+		return nil
+	}
+	text, err = FUCK(arr, art.Reverse.Shadow, mapAscii)
+	if err == nil {
+		art.Result = text[:len(text)-1]
+		return nil
+	}
+	text, err = FUCK(arr, art.Reverse.Thinkertoy, mapAscii)
+	if err == nil {
+		art.Result = text[:len(text)-1]
+		return nil
+	}
+
+	return err
+}
+func FUCK(arr, style []string, mapAscii map[rune]int) (string, error) {
 	text := ""
 	counter := 0
 	for j := 0; j < len(arr); j += 8 {
@@ -49,7 +68,7 @@ func (art *ArtObjects) ReverseAscii() error {
 					for f := ' '; f <= '~'; f++ {
 						as := true
 						for a := 0; a < 8; a++ {
-							if arr[j+a][prevLetter:i+1] != art.Reverse.Standard[(mapAscii[f])+a] {
+							if arr[j+a][prevLetter:i+1] != style[(mapAscii[f])+a] {
 								as = false
 							}
 						}
@@ -66,55 +85,14 @@ func (art *ArtObjects) ReverseAscii() error {
 		text += "\n"
 
 	}
+	text = text[:len(text)-1]
 	if counter != len(text)-1 {
-		return errors.New("invalid ahhahah")
+
+		return "", errors.New("Asdas")
 	}
-	art.Result = text[:len(text)-1]
-	return nil
+	return text, nil
 }
-func FUCK(arr []string, counter int, text string) {
-	for j := 0; j < len(arr); j += 8 {
-		if len(arr[j]) == 0 {
-			text += "\n"
-			j -= 7
-			counter++
-			continue
-		}
 
-		prevLetter := 0
-		for i, el := range arr[j] {
-			var valid bool
-			if j < len(arr)-8 && el == ' ' {
-				for c := 1; c < 8; c++ {
-					if byte(el) != arr[j+c][i] {
-						valid = true
-						break
-					}
-				}
-
-				if !valid {
-
-					for f := ' '; f <= '~'; f++ {
-						as := true
-						for a := 0; a < 8; a++ {
-							if arr[j+a][prevLetter:i+1] != art.Reverse.Standard[(mapAscii[f])+a] {
-								as = false
-							}
-						}
-						if as {
-							prevLetter = i + 1
-							text += string(f)
-							counter++
-						}
-
-					}
-				}
-			}
-		}
-		text += "\n"
-
-	}
-}
 func (art *ArtObjects) getFsForCompare() error {
 
 	std, err := os.ReadFile(
